c7a505c5a490caa0d999cd2b2bf3482509aa324b
yazminD21 <yaaraujodiaz1@buffs.wtamu.edu>
2026-01-25 21:11:41 -0600

    Submit Homework 1

---
+++ homework1.cs
@@ -0,0 +1,16 @@
+﻿// Program class moved to homework1.cs
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double X = 2.5;
+        double Y = 3.3;
+
+        double Z = 4 * X * X + 3 * Y;
+
+        Console.WriteLine($"When X = {X} and Y = {Y}, the value of Z is {Z:F2}");
+    }
+}
+
