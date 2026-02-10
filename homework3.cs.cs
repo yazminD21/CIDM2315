a4d63d87d7270db7bd7b3eb9434b76d8c9b8d101
yazminD21 <yaaraujodiaz1@buffs.wtamu.edu>
2026-02-09 23:14:13 -0600

    Homework 2
    Im not sure if i did this correctly

---
+++ homework3.cs.cs
@@ -0,0 +1,40 @@
+﻿namespace Homework3;
+class Program
+{
+    static void Main(string[ ] args)
+    {
+        // Code for Q1
+        Console.Write("Enter integer N: ");
+        string? input = Console.ReadLine();
+        if (!long.TryParse(input, out long n))
+        {
+            Console.WriteLine("Invalid input.");
+            return;
+        }
+
+        bool prime = IsPrime(n);
+        Console.WriteLine($"{n} is {(prime ? "prime" : "non-prime")}");
+
+        // Code for Q2
+        Console.Write("Enter integer N: ");
+        string? inputQ2 = Console.ReadLine();
+        if (!int.TryParse(inputQ2, out int n2))
+        {
+            Console.WriteLine("Invalid input.");
+            return;
+        }
+
+        for (int i = 0; i < n2; i++)
+        {
+            for (int j = 0; j < n2; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }}
+
+        // Code for Bonus question
+    
+
+    
+}
